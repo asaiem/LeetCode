ddc1615d2b1f28d63ba7ca04534b8c99c3e4bfb1
Abdelrahman Mohamed El Sayem <67355359+asaiem@users.noreply.github.com>
2023-08-12 05:33:25 +0300

    Time: 56 ms (88.93%), Space: 45.4 MB (5.12%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let n = init;
+    return{
+        increment:()=>{
+            return n = n + 1
+        },
+        decrement:()=>{
+             n = n - 1
+            return n;
+        },
+        reset:()=>{
+            n = init
+            return n
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
