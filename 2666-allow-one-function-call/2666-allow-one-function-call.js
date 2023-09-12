50a539c0f1a7b1801306e14c761638a9cf5e294d
Abdelrahman  El Sayem <67355359+asaiem@users.noreply.github.com>
2023-09-12 22:22:59 +0300

    Time: 43 ms (95.09%), Space: 41.9 MB (51.25%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    // this is destructuring the arrays into array 
+    let isCalled = false;
+    return function(...args){
+        if(isCalled === false){
+            isCalled = true;
+            return fn(...args)
+        }else{
+            return undefined
+        }
+        
+        
+        
+        
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
